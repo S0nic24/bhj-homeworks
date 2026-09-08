c6bbabb681fddcd8e854f0d3f11e99b82dc4cfbc
Sofia <sofiafinik0906@gmail.com>
2026-09-08 21:56:53 +0300

    event objects two

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,16 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keyup', (event) => {
+        const expectedChar = this.currentSymbol.textContent.toLowerCase();
+        const typedChar = event.key.toLowerCase();
+        
+        if (expectedChar === typedChar) {
+            this.success();
+        } else {
+            this.fail();
+        }
+    });
   }
 
   success() {
@@ -91,4 +93,3 @@ class Game {
 }
 
 new Game(document.getElementById('game'))
-
