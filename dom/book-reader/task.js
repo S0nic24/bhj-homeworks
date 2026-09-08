c393d68ab7ad851225007a2bc9d0bca7573b987f
Sofia <sofiafinik0906@gmail.com>
2026-09-08 23:06:11 +0300

    dom three

---
+++ dom/book-reader/task.js
@@ -0,0 +1,20 @@
+const book = document.getElementById('book');
+const fontSizeControls = document.querySelectorAll('.font-size');
+
+fontSizeControls.forEach(control => {
+    control.addEventListener('click', function(event) {
+        event.preventDefault();
+        
+        fontSizeControls.forEach(c => c.classList.remove('font-size_active'));
+        this.classList.add('font-size_active');
+        
+        book.classList.remove('book_fs-big', 'book_fs-small');
+        
+        const size = this.dataset.size;
+        if (size === 'big') {
+            book.classList.add('book_fs-big');
+        } else if (size === 'small') {
+            book.classList.add('book_fs-small');
+        }
+    });
+});
